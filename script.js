f782da4a3b92c550e449e0cd3257410d50199afd
GunturDev <161219490+GunturDeveloper@users.noreply.github.com>
2024-08-13 18:46:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function searchGoogle() {
     var query = document.getElementById('searchQuery').value;
     var googleSearchUrl = "https://www.google.com/search?q=" + encodeURIComponent(query);
-    document.getElementById('searchResultsIframe').src = googleSearchUrl;
+    window.location.href = googleSearchUrl;
 }
